f63eaa9efe6a0f7c158b364df48682899d1541c4
neliya_kiyasova <neliya2005@mail.ru>
2025-03-27 09:51:43 +0300

    Последнее изменение

--- Practice6/serverfront.js
+++ Practice6/serverfront.js
@@ -15,5 +15,5 @@ app.get('/admin', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:`, PORT);
 });
